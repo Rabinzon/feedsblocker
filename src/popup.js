270b715b5a09b03918ac2451d74bcc1e86e6c9a3
rabinzon <ildarrazin@gmail.com>
2016-10-31 18:01:38 +0300

    feat(popup): add updateFormData

--- src/popup.js
+++ src/popup.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import {map} from 'ramda';
 
-const getStore = () =>
-	JSON.parse(localStorage.getItem('data'));
+const getStore = key =>
+	JSON.parse(localStorage.getItem(key));
 
 const check = elem => {
 	if (elem.url instanceof Array) {
@@ -14,8 +14,19 @@ const check = elem => {
 
 const listify = map(check);
 
+const clearFormData = () =>
+	localStorage.removeItem('formData');
+
+const getFormData = () => {
+	const formData = getStore('formData');
+	if (formData) {
+		return formData;
+	}
+	return {url: '', redirect: ''};
+};
+
 const addUrls = function () {
-	const store = getStore();
+	const store = getStore('data');
 	const newUrl = {
 		url: [this.form.url],
 		redirect: this.form.redirect
@@ -23,17 +34,27 @@ const addUrls = function () {
 
 	store.push(newUrl);
 	localStorage.setItem('data', JSON.stringify(store));
-	this.list = listify(getStore());
+	this.list = listify(getStore('data'));
+	clearFormData();
+};
+
+const updateFormData = function () {
+	const formData = {
+		url: this.form.url,
+		redirect: this.form.redirect
+	};
+
+	localStorage.setItem('formData', JSON.stringify(formData));
 };
 
 export const app = new Vue({
 	el: '.app',
 	data: {
-		list: listify(getStore()),
-		form: {
-			url: '',
-			redirect: ''
-		}
+		list: listify(getStore('data')),
+		form: getFormData()
 	},
-	methods: {addUrls}
+	methods: {
+		addUrls,
+		updateFormData
+	}
 });
