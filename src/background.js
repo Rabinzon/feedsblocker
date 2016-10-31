28fff517ec456d94fd0c56f864596141b3ea4688
rabinzon <ildarrazin@gmail.com>
2016-10-31 10:55:53 +0300

    init project

---
+++ src/background.js
@@ -0,0 +1,38 @@
+var that = this;
+
+var domens = [
+	{
+		url: [
+			'https://www.youtube.com/',
+			'https://www.youtube.com/?gl=RU&hl=ru',
+			'https://www.youtube.com/?gl=RU&=ru'
+		],
+		rederect: 'https://www.youtube.com/feed/subscriptions'
+	},
+	{
+		url: [
+			'https://vk.com/feed',
+			'https://new.vk.com/feed'
+		],
+		rederect: 'https://vk.com/id0',
+	}
+];
+
+chrome.tabs.onUpdated.addListener(function(data) {
+	chrome.tabs.getSelected(null, function(tab) {
+		alert(tab);
+	});
+});
+
+function find(tab) {
+	return _.find(domens, function(obj) {
+		if(index(obj, tab.url) > -1){
+			return obj;
+		}
+	});
+}
+function index(obj, url){
+	return _.findIndex(obj.url, function(s) {
+		return s == url;
+	})
+}
