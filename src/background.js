2c64267549ac06391944c8b0778ed1236b1aeb61
rabinzon <ildarrazin@gmail.com>
2016-10-31 14:16:31 +0300

    feat(bg): add redirect logic

--- src/background.js
+++ src/background.js
@@ -1,5 +1,23 @@
-chrome.tabs.onUpdated.addListener(() => {
-	chrome.tabs.getSelected(null, () => {
-	});
-});
+import {find, equals} from 'ramda';
+import data from './data.json';
 
+localStorage.setItem('data', JSON.stringify(data));
+
+const getData = () =>
+	JSON.parse(localStorage.getItem('data'));
+
+const redirect = (id, url) =>
+	chrome.tabs.update(id, {url});
+
+const findUrl = tab =>
+	find(elem => find(equals(tab.url))(elem.url));
+
+const checkTab = tab => {
+	const site = findUrl(tab)(getData());
+	if (site) {
+		redirect(tab.id, site.redirect);
+	}
+};
+
+chrome.tabs.onUpdated.addListener(() =>
+	chrome.tabs.getSelected(null, checkTab));
